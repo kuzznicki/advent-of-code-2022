18075536750144a0488c3a46c8a6cb31c116b2bd
kuzznicki <kuznicki@protonmail.com>
2022-12-02 10:28:48 +0100

    fix end-of-line to work on windows

--- day_01/solution.js
+++ day_01/solution.js
@@ -1,3 +1,4 @@
+import { EOL } from "os";
 import { inputFileToArray, inputToArray, sum } from "../utils.js";
 
 const [answer1, answer2] = solve();
@@ -5,8 +6,8 @@ console.log('PART ONE: ' + answer1);
 console.log('PART TWO: ' + answer2);
 
 function solve() {
-    const elfInputs = inputFileToArray('input.txt', '\n\n');
-    const elfBackpacks = elfInputs.map(input => inputToArray(input, '\n', e => Number(e)));
+    const elfInputs = inputFileToArray('input.txt', EOL.repeat(2));
+    const elfBackpacks = elfInputs.map(input => inputToArray(input, EOL, e => Number(e)));
     const elfTotals = elfBackpacks.map(backpack => sum(backpack));
     elfTotals.sort((a, b) => b - a);
     return [elfTotals[0], sum(elfTotals.slice(0, 3))];
