e98b6bdaf7feae4f9f7d9c52c0b63f3db041f0cb
kuzznicki <kuznicki@protonmail.com>
2022-12-07 20:11:27 +0100

    day 4

---
+++ day_04/solution.js
@@ -0,0 +1,32 @@
+import { EOL } from "os";
+import { inputFileToArray, intersect, range, unique } from "../utils.js";
+
+const [answer1, answer2] = solve();
+console.log('PART ONE: ' + answer1);
+console.log('PART TWO: ' + answer2);
+
+function solve() {
+    const pairs = inputFileToArray('input.txt', EOL);
+    let fullyContains = 0;
+    let overlaps = 0;
+
+    for (const pair of pairs) {
+        const ranges = pair.split(',');
+        const [areas1, areas2] = ranges.map(r => {
+            const [first, last] = r.split('-');
+            return range(first, last);
+        });
+
+        const shared = intersect(areas1, areas2);
+        
+        if (shared.length) {
+            overlaps++;
+        }
+
+        if ([areas1.length, areas2.length].includes(shared.length)) {
+            fullyContains++;
+        }
+    }
+
+    return [fullyContains, overlaps];
+}
--- utils.js
+++ utils.js
@@ -17,3 +17,13 @@ export function sum(arr) {
 export function unique(arr) {
     return arr.filter((e, i, a) => a.indexOf(e) === i);
 }
+
+export function intersect(arr1, arr2) {
+    return arr1.filter(e => arr2.includes(e));
+}
+
+export function range(from, to) {
+    if (!to && to !== 0) return range(0, from);
+    [from, to] = [from, to].map(n => +n);
+    return new Array(+to - +from + 1).fill(1).map((_, i) => +from + i);
+}
