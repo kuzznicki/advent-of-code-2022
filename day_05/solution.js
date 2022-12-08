0585028e0b5128f01a34cf28229f161758a7c059
kuzznicki <kuznicki@protonmail.com>
2022-12-08 07:43:57 +0100

    day 5

---
+++ day_05/solution.js
@@ -0,0 +1,53 @@
+import { EOL } from "os";
+import { inputFileToArray, chunk } from "../utils.js";
+
+const [answer1, answer2] = solve();
+console.log('PART ONE: ' + answer1);
+console.log('PART TWO: ' + answer2);
+
+function solve() {
+    const lines = inputFileToArray('input.txt', EOL);
+    const [crates, moves] = readInputLines(lines);
+    return [
+        cratesToSolution(moveCrates(crates, moves, 'CrateMover 9000')),
+        cratesToSolution(moveCrates(crates, moves, 'CrateMover 9001'))
+    ];
+}
+
+function readInputLines(inputLines) {
+    const crates = [], moves = [];
+
+    for (const line of inputLines) {
+        if (line.includes('[')) {
+            const chunks = chunk(line.split(''), 4);
+
+            chunks.forEach((chunq, i) => {
+                if (chunq[0] !== '[') return;
+                crates[i] = (crates[i] || []);
+                crates[i].push(chunq[1]);
+            });
+
+        } else if (line.slice(0, 4) === 'move') {
+            const [move, from, to] = line.split(' ').map(Number).filter(e => !isNaN(e));
+            moves.push({ move, from, to });
+        }
+    }
+
+    return [crates.map(col => col.reverse()), moves];
+}
+
+function moveCrates(initialCrates, moves, crane) {
+    const crates = initialCrates.map(col => [...col]);
+    
+    moves.forEach(({ move, from, to }) => {
+        const movingCrates = crates[from - 1].splice(-move);
+        if (crane !== 'CrateMover 9001') movingCrates.reverse();
+        crates[to - 1].push(...movingCrates);
+    });
+
+    return crates;
+}
+
+function cratesToSolution(crates) {
+    return crates.map(cratesColumn => cratesColumn.slice(-1)).join('');
+}
--- utils.js
+++ utils.js
@@ -22,6 +22,13 @@ export function intersect(arr1, arr2) {
     return arr1.filter(e => arr2.includes(e));
 }
 
+export function chunk(arr, size) {
+    const chunks = [];
+    const source = [...arr];
+    while (source.length) chunks.push(source.splice(0, size));
+    return chunks;
+}
+
 export function range(from, to) {
     if (!to && to !== 0) return range(0, from);
     [from, to] = [from, to].map(n => +n);
